094ca8513ae6dcab9741901971fb8d904bf15b6a
Ythsc0529 <hsc0529.myself@gmail.com>
2026-04-14 14:01:17 +0800

    version to 1.6.0

--- js/core/version.js
+++ js/core/version.js
@@ -2,6 +2,6 @@
  * Neon Card Game - Version Control
  * This file defines the current client version.
  */
-const APP_VERSION = "0.2.0"; // Initial Version after adding this system
+const APP_VERSION = "1.6.0"; // Initial Version after adding this system
 
 window.APP_VERSION = APP_VERSION;
