a8257890a338b48613da04476ce826b94755370f
Ythsc0529 <hsc0529.myself@gmail.com>
2026-04-28 19:38:36 +0800

    Fix update button binding to prevent reload

--- www/js/core/version.js
+++ www/js/core/version.js
@@ -2,5 +2,5 @@
  * Neon Card Game - Version Control
  * This file defines the current client version.
  */
-const APP_VERSION = "1.6.4";
+const APP_VERSION = "1.6.5";
 window.APP_VERSION = APP_VERSION;
